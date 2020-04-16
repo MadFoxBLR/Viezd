f84ea7693b58039d001467e26e14f281166f5bde
MadFoxBLR <56344115+MadFoxBLR@users.noreply.github.com>
2020-04-16 16:24:48 +0300

    add mask

---
+++ source/js/common.js
@@ -0,0 +1,10 @@
+
+/*$(document).ready(function () {
+    $("#example1").inputmask("+375-99-999-99-99");
+    $("#example2").inputmask("+375-99-999-99-99");
+});*/
+
+$(document).ready(function () {
+    $(".text").mask("+375(99)999-99-99");
+    $(".form__text").mask("+375(99)999-99-99");
+})
